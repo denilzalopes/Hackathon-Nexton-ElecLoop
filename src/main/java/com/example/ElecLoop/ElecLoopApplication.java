7fae68210473d0687a7bd9f9fb2a65761e3c434d
Denilza <122271712+denilzalopes@users.noreply.github.com>
2024-12-04 22:41:07 +0100

    first commit

---
+++ src/main/java/com/example/ElecLoop/ElecLoopApplication.java
@@ -0,0 +1,13 @@
+package com.example.ElecLoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElecLoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElecLoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ElecLoop/ElecLoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ElecLoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElecLoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
